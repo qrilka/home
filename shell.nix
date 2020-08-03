349f75ff84095e074d221ce8131bbb0639357ed2
Kirill Zaborsky <qrilka@gmail.com>
2020-08-03 12:32:20 +0300

    Fix urxvt terminfo on Gentoo

--- shell.nix
+++ shell.nix
@@ -18,6 +18,6 @@ in pkgs.mkShell rec {
   shellHook = ''
     export NIX_PATH="nixpkgs=${nixpkgs}:home-manager=${sources."home-manager"}"
     export HOME_MANAGER_CONFIG="./home.nix"
+    export TERMINFO_DIRS="/usr/lib/terminfo"
   '';
-
 }
